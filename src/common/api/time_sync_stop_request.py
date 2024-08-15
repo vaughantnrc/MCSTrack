309910a080f11ff06af1e9e8cbe564a9ba8dd5de
Thomas Vaughan <thomas.vaughan@cnrc-nrc.gc.ca>
2024-08-15 13:06:26 -0400

    BUG: Fix TimeSyncStopRequest not working due to incorrect type identifier

--- src/common/api/time_sync_stop_request.py
+++ src/common/api/time_sync_stop_request.py
@@ -5,6 +5,6 @@ from pydantic import Field
 class TimeSyncStopRequest(MCTRequest):
     @staticmethod
     def parsable_type_identifier() -> str:
-        return "time_sync_start_request"
+        return "time_sync_stop_request"
 
     parsable_type: str = Field(default=parsable_type_identifier(), const=True)
