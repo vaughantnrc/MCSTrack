a51cfccbc5f6cde86bfb9d34bd35859acdcbd6d6
Thomas Vaughan <thomas.vaughan@cnrc-nrc.gc.ca>
2024-08-15 13:11:24 -0400

    MNT: Remove unused function referring to non-existing member

--- src/common/api/timestamp_get_response.py
+++ src/common/api/timestamp_get_response.py
@@ -1,6 +1,4 @@
-import datetime
 from .mct_response import MCTResponse
-from src.common.structures.status_message import StatusMessage
 from pydantic import Field
 
 
@@ -9,9 +7,6 @@ class TimestampGetResponse(MCTResponse):
     @staticmethod
     def parsable_type_identifier() -> str:
         return "timestamp_get_response"
-
-    def timestamp_utc(self):
-        return datetime.datetime.fromisoformat(self.timestamp_utc_iso8601)
 
     parsable_type: str = Field(default=parsable_type_identifier(), const=True)
     requester_timestamp_utc_iso8601: str = Field()
